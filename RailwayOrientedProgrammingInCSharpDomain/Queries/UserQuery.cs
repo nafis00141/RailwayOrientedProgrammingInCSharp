b00d95fa2b4b37aed1062a9980e0de0bb15fbc40
nafis00141 <nafis0014@gmail.com>
2020-08-09 03:33:09 +0600

    Added User Query

---
+++ RailwayOrientedProgrammingInCSharpDomain/Queries/UserQuery.cs
@@ -0,0 +1,44 @@
+﻿using RailwayOrientedProgrammingInCSharpDomain.Data;
+using RailwayOrientedProgrammingInCSharpDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RailwayOrientedProgrammingInCSharpDomain.Queries
+{
+  public interface IUserQuery
+  {
+    User GetUser(long userId);
+    bool UpdateUser(User user);
+  }
+
+  public class UserQuery : ExceptionHelper, IUserQuery
+  {
+    private IEnumerable<User> _users;
+
+    public UserQuery()
+    {
+      // mocking a database
+      _users = new User[]
+      {
+        new User() { Id = 1, Name = "Nafis Islam", Email = "nafis@example.com", Password = "12345"},
+        new User() { Id = 2, Name = "Sajid Ahmed", Email = "sajid@example.com", Password = "12345"},
+        new User() { Id = 3, Name = "Redone Ahmed", Email = "red1@example.com", Password = "12345"}
+      };
+    }
+
+    public User GetUser(long userId) =>
+      _users.FirstOrDefault(u => u.Id == userId);
+
+    public bool UpdateUser(User user) =>
+      DoUnitOfWork(() => UpdateUserData(user));
+
+    private void UpdateUserData(User data)
+    {
+      // mock update db
+      var user = _users.First(u => u.Id == data.Id);
+      user.Name = data.Name;
+      user.Email = data.Email;
+    }
+  }
+}

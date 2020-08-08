2233dc913db8beba5b8aac5c5e981091fb782d2c
nafis00141 <nafis0014@gmail.com>
2020-08-09 03:34:45 +0600

    Added Web Api

---
+++ RailwayOrientedProgrammingInCSharpWebApi/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RailwayOrientedProgrammingInCSharpDomain.Data;
+using RailwayOrientedProgrammingInCSharpDomain.Models;
+using RailwayOrientedProgrammingInCSharpDomain.Results;
+
+namespace RailwayOrientedProgrammingInCSharp.Controllers
+{
+  public class BaseController : ControllerBase
+  {
+    protected IActionResult CustomBadRequest(ErrorType errorType) =>
+      errorType
+      .ErrorTypesToErrorMessage()
+      .Then(GenericResponse.CreateGenericResponse)
+      .Then(BadRequest);
+  }
+}
---
+++ RailwayOrientedProgrammingInCSharpWebApi/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RailwayOrientedProgrammingInCSharpDomain.Models;
+using RailwayOrientedProgrammingInCSharpDomain.Services;
+
+namespace RailwayOrientedProgrammingInCSharp.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UserController : BaseController
+  {
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+      _userService = userService;
+    }
+
+    [HttpPost("update")]
+    public IActionResult Update(UpdateUserDto updateUserDto) =>
+      _userService.UpdateUser(updateUserDto)
+      .Match(
+        Ok,
+        CustomBadRequest
+      );
+  }
+}

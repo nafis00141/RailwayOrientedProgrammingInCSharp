97fb773eab382859f81319b5248460f10529464f
nafis00141 <nafis0014@gmail.com>
2020-08-09 03:31:57 +0600

    Added Domain

---
+++ RailwayOrientedProgrammingInCSharpDomain/Data/ExceptionHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RailwayOrientedProgrammingInCSharpDomain.Data
+{
+  public class ExceptionHelper
+  {
+    protected bool DoUnitOfWork(Action f)
+    {
+      try
+      {
+        f();
+        return true;
+      }
+      catch (Exception)
+      {
+        return false;
+      }
+    }
+
+    protected bool DoUnitOfWork(Func<bool> f)
+    {
+      try
+      {
+        return f();
+      }
+      catch (Exception)
+      {
+        return false;
+      }
+    }
+  }
+}
